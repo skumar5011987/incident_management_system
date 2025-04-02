6ec579ae201523c5688c4e6c60cd3f2a5db8ca2a
skumar5011987 <72865604+skumar5011987@users.noreply.github.com>
2025-04-02 16:05:58 +0530

    Update email backend in settingspy

--- backend/ims/settings.py
+++ backend/ims/settings.py
@@ -151,14 +151,7 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "skumar5011987@gmail.com"
-EMAIL_HOST_PASSWORD = "reez fzsr zsex utev"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # JWT setting
 SIMPLE_JWT = {
